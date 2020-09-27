ce6da0459be83c6255028628c287c09d130a76d9
kranks40 <eng_peters20@yahoo.com>
2020-09-27 18:27:02 -0400

    Created login and spotify components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  // Run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash ='';
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+      spotify.getMe().then(user => {
+        
+      })
+    }
+  }, []);
+
   return (
     <div className="app">
-      <h1>Home Page</h1>
+      {
+        token ? (
+          <h1>I am logged in</h1>
+        ) : (
+          <Login/>
+        )
+      }
+      
     </div>
   );
 }
